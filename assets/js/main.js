6414ec8001cc58e968d9a5e7f0a718f14f8d5817
syrinesmati <syrine.smati@insat.ucar.tn>
2024-04-03 01:38:03 +0000

    inittial commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav__menu'),
+      navToggle = document.getElementById('nav__toggle'),
+      navClose = document.getElementById('nav__close')
+
+/* Menu show */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
